6ff58e05c4473ad006998f3b162f6c6304311031
Carlos C Barajas Jr <carlos.barajas@bitandbyteideas.com>
2026-01-02 13:52:43 -0800

    feat: create landing page base (#2)

---
+++ src/app/features/home/components/services-overview/services-overview.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-services-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './services-overview.html',
+  styleUrl: './services-overview.css',
+})
+export class ServicesOverview {}
