fc02de5575ea2ac8114f9649bf728ff8e54a1fb1
Carlos Barajas <carlos@glutgut.com>
2026-01-03 03:39:38 +0000

    feat: create services page, with placeholders

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,9 @@ export const routes: Routes = [
     path: '',
     loadComponent: () => import('./features/home/pages/home-page/home-page').then(m => m.HomePage),
   },
+  {
+    path: 'services',
+    loadComponent: () =>
+      import('./features/services/pages/services-page/services-page').then(m => m.ServicesPage),
+  },
 ];
