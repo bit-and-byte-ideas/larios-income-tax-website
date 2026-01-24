74c79ee07f32a15b7ce844717280a8bb921a56fa
Carlos Barajas <carlos@glutgut.com>
2026-01-24 16:52:39 +0000

    fix: adjust tf format & add tools to devcontainer

--- deploy/environments/prod/main.tf
+++ deploy/environments/prod/main.tf
@@ -15,15 +15,15 @@ provider "azurerm" {
 module "static_web_app" {
   source = "../../modules/static-web-app"
 
-  resource_group_name = var.resource_group_name
-  location            = var.location
-  static_web_app_name = var.static_web_app_name
-  app_insights_name   = var.app_insights_name
-  sku_tier            = var.sku_tier
-  sku_size            = var.sku_size
+  resource_group_name         = var.resource_group_name
+  location                    = var.location
+  static_web_app_name         = var.static_web_app_name
+  app_insights_name           = var.app_insights_name
+  sku_tier                    = var.sku_tier
+  sku_size                    = var.sku_size
   enable_application_insights = var.enable_application_insights
-  custom_domain       = var.custom_domain
-  app_settings        = var.app_settings
+  custom_domain               = var.custom_domain
+  app_settings                = var.app_settings
 
   tags = {
     Environment = "prod"
