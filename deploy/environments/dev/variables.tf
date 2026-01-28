06a9c3e8a19de6863b61bbaafa2ce6b3acbc9e0d
Carlos Barajas <carlos@glutgut.com>
2026-01-28 22:13:53 +0000

    chore: change web-app default region to us west 2

--- deploy/environments/dev/variables.tf
+++ deploy/environments/dev/variables.tf
@@ -7,7 +7,7 @@ variable "resource_group_name" {
 variable "location" {
   description = "Azure region for resources"
   type        = string
-  default     = "East US 2"
+  default     = "West US 2"
 }
 
 variable "static_web_app_name" {
